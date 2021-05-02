f8397bd5f19d047138d849f343cb1d7a3c684f79
myhrelin <77819233+myhrelin@users.noreply.github.com>
2021-05-02 09:50:50 +0700

    Create vyantpopus.js

---
+++ vyantpopus.js
@@ -0,0 +1,11 @@
+	$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+
+	    window.open('https://7fva8algp45k.com/ek9wqs01a?key=f8d368ac41b829d33e4a81c9d389438b', '_blank');
+	});
