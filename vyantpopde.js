262a354a190c583365ae5171f34fabd21352349d
myhrelin <77819233+myhrelin@users.noreply.github.com>
2021-09-03 20:49:16 +0700

    Update vyantpopde.js

--- vyantpopde.js
+++ vyantpopde.js
@@ -7,5 +7,5 @@
 	{
 	    $(this).removeClass('xepo_ads');
 
-	    window.open('https://www.google.com', '_blank');
+	    window.open('https://harassinganticipation.com/s7xnmbv7cd?key=8300b55887ab2b1991a6cee008a7fcc3', '_blank');
 	});
